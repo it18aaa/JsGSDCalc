9321c71148f68dcfbdb5561c5ba8c6015d16e3c2
Ian <i.thomson@ljmu.ac.uk>
2023-01-09 16:26:49 +0000

    installed sass into libs

--- src/print.js
+++ src/print.js
@@ -1,19 +1,10 @@
 function printMe() {
-
-
     const lastChild = document.body.lastChild;
-
-    
-
-    //const div = document.createElement("div");
     const p = document.createElement("p");
     p.innerHTML = `${Date.now()}`;
-    //div.appendChild(p);
-    //document.body.appendChild(div); 
 
     lastChild.appendChild(p);
-
-    
+  
 
 }
 
